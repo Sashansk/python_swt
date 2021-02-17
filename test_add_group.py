712d1da2f69c022351565577c38dfd169c5356bd
Sashansk <zuikof@bk.ru>
2021-02-18 01:18:52 +0700

    add group.py(not in git) add sleep

--- test_add_group.py
+++ test_add_group.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from selenium.common.exceptions import NoSuchElementException
-import unittest, time
 from selenium import webdriver
+from group import Group
+import unittest, time
+
 
 def is_alert_present(wd):
     try:
@@ -17,40 +19,51 @@ class TestAddGroup(unittest.TestCase):
         url = "C:\\Users\\User\\Study\\Soft\\chromedriver.exe"
         self.wd = webdriver.Chrome(url)
 
-
-    #TEST1 Create new group
+    # TEST1 Create new group
     def test_add_group(self):
         wd = self.wd
         self.open_homepage(wd)
         self.login(wd, uname="admin", passwd="secret")
         self.open_group_page(wd)
         self.create_new_group(wd)
-        self.fill_group_content(wd, gr_name="group10", gr_params="grouplogo", gr_comment="comment")
+        self.fill_group_content(wd, Group(gr_name="group11", gr_params="grouplogo", gr_comment="comment"))
         self.submit_group(wd)
         self.open_group_page(wd)
         time.sleep(2)
         self.logout_addressbook(wd)
 
-    #Metods list
+    def test_add_null_group(self):
+        wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd, uname="admin", passwd="secret")
+        self.open_group_page(wd)
+        self.create_new_group(wd)
+        self.fill_group_content(wd, Group(gr_name="", gr_params="", gr_comment=""))
+        self.submit_group(wd)
+        self.open_group_page(wd)
+        time.sleep(2)
+        self.logout_addressbook(wd)
+
+    # Methods list
     def logout_addressbook(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
     def submit_group(self, wd):
         wd.find_element_by_name("submit").click()
 
-    def fill_group_content(self, wd, gr_name, gr_params, gr_comment):
+    def fill_group_content(self, wd, group):
         # Fill group name
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(gr_name)
+        wd.find_element_by_name("group_name").send_keys(group.gr_name)
         # Fill group params
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(gr_params)
+        wd.find_element_by_name("group_header").send_keys(group.gr_params)
         #  Fill group comment
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(gr_comment)
+        wd.find_element_by_name("group_footer").send_keys(group.gr_comment)
 
     def create_new_group(self, wd):
         wd.find_element_by_name("new").click()
@@ -71,13 +84,16 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def tearDown(self):
         self.wd.close()
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()

6fd284b0f49ff3c72cad32d0d8457166d77bea6d
Sashansk <zuikof@bk.ru>
2021-02-18 00:21:22 +0700

    add test methods

--- test_add_group.py
+++ test_add_group.py
@@ -10,7 +10,6 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 from selenium import webdriver
 
-
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -24,22 +23,27 @@ class TestAddGroup(unittest.TestCase):
         url = "C:\\Users\\User\\Study\\Soft\\chromedriver.exe"
         self.wd = webdriver.Chrome(url)
 
+
+    #TEST1 Create new group
     def test_add_group(self):
         wd = self.wd
-        # open page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_xpath("//body").click()
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//body").click()
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # Open group page
-        wd.find_element_by_link_text("groups").click()
-        # Create new group
-        wd.find_element_by_name("new").click()
+        self.open_homepage(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_new_group(wd)
+        self.fill_group_content(wd)
+        self.submit_group(wd)
+        self.open_group_page(wd)
+        self.logout_addressbook(wd)
+
+    #Metods list
+    def logout_addressbook(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def submit_group(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def fill_group_content(self, wd):
         # Fill group name
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -52,13 +56,25 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("comment")
-        # Submit
-        wd.find_element_by_name("submit").click()
-        # Groups page
-        wd.find_element_by_link_text("group page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def create_new_group(self, wd):
+        wd.find_element_by_name("new").click()
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_xpath("//body").click()
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//body").click()
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -67,7 +83,6 @@ class TestAddGroup(unittest.TestCase):
     def tearDown(self):
         self.wd.close()
         self.wd.quit()
-
 
 if __name__ == "__main__":
     unittest.main()

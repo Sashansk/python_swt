cd1f0f1702a41aa226fb871d06a9ba5bd4a194b7
Sashansk <zuikof@bk.ru>
2021-02-22 16:09:54 +0700

    add_person.py

---
+++ add_person.py
@@ -0,0 +1,28 @@
+class AddPerson:
+
+    def __init__(self, f_name, m_name, l_name, nick, title, company, address, h_phone, mob_phone, work_phone, fax,
+                 mail1, mail2, mail3, site, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes):
+        self.f_name = f_name
+        self.m_name = m_name
+        self.l_name = l_name
+        self.nick = nick
+        self.title = title
+        self.company = company
+        self.address = address
+        self.h_phone = h_phone
+        self.mob_phone = mob_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.mail1 = mail1
+        self.mail2 = mail2
+        self.mail3 = mail3
+        self.site = site
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
